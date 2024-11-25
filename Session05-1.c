a3bd5c6da59cee2458b6cc85e7c6401a51440685
TranQuoc2k6 <qatran2k6@gmail.com>
2024-11-25 23:53:46 +0700

    Upload session05-1

---
+++ Session05-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	//buoc1: khai bao bien i 
+	int i;
+	//buoc2: xu li dieu kien 
+	for(i=100;i>0;i--){
+		printf("%d\n",i);
+	}
+	//buoc5: ket thuc chuong trinh
+	return 0;
+}
